8bb19cb2ef0ef0635e5f50613a6251eee4abf5ca
Jodi Chandra <70di.chandra@gmail.com>
2024-03-26 13:52:23 +0700

    Update

---
+++ src/Antrian/Antrian.php
@@ -0,0 +1,28 @@
+<?php
+namespace Bridging\Bpjs\Antrian;
+
+use Bridging\Bpjs\BpjsService;
+
+class Antrian extends BpjsService
+{
+    public function tambahAntrianJKN($data = [])
+    {
+        $response = $this->post('antrean/add', $data);
+        return json_decode($response, true);
+    }
+
+    public function updateWaktuAntrian($data = [])
+    {
+        $response = $this->post('antrean/updatewaktu', $data);
+        return json_decode($response, true);
+    }
+
+    public function batalAntrian($data = [])
+    {
+        $response = $this->post('antrean/batal', $data);
+        return json_decode($response, true);
+    }
+
+    
+
+}
---
+++ src/VClaim/RencanaKontrol.php
@@ -0,0 +1,76 @@
+<?php
+namespace Bridging\Bpjs\VClaim;
+use Bridging\Bpjs\BpjsService;
+
+class RencanaKontrol extends BpjsService
+{
+    /**
+     * Rencana Kontrol
+     */
+    public function insertKontrol($data = [])
+    {
+        $response = $this->post('RencanaKontrol/insert', $data);
+        return json_decode($response, true);
+    }
+    public function updateKontrol($data = [])
+    {
+        $response = $this->put('RencanaKontrol/Update', $data);
+        return json_decode($response, true);
+    }
+    public function deleteKontrol($data = [])
+    {
+        $response = $this->delete('RencanaKontrol/Delete', $data);
+        return json_decode($response, true);
+    }
+
+    /**
+     * Surat Perintah Rawat Inap (SPRI)
+     */
+    public function insertSPRI($data = [])
+    {
+        $response = $this->post('RencanaKontrol/InsertSPRI', $data);
+        return json_decode($response, true);
+    }
+    public function updateSPRI($data = [])
+    {
+        $response = $this->put('RencanaKontrol/UpdateSPRI', $data);
+        return json_decode($response, true);
+    }
+    
+
+    public function cariSEPKontrol($noSep)
+    {
+        $response = $this->get('RencanaKontrol/nosep/'.$noSep);
+        return json_decode($response, true);
+    }
+
+    public function cariNomorKontrol($noSuratKontrol)
+    {
+        $response = $this->get('RencanaKontrol/noSuratKontrol/'.$noSuratKontrol);
+        return json_decode($response, true);
+    }
+
+    public function cariNomorKontrolByNoka($bulan, $tahun, $noKa, $filter)
+    {
+        $response = $this->get('RencanaKontrol/ListRencanaKontrol/Bulan/'.$bulan.'/Tahun/'.$tahun.'/Nokartu/'.$noKa.'/filter/'.$filter);
+        return json_decode($response, true);
+    }
+    
+    public function listRencanaKontrol($tglAwal, $tglAkhir, $filter)
+    {
+        $response = $this->get('RencanaKontrol/ListRencanaKontrol/tglAwal/'.$tglAwal.'/tglAkhir/'.$tglAkhir.'/filter/'.$filter);
+        return json_decode($response, true);
+    }
+    
+    public function listPoliKontrol($JnsKontrol, $nomor, $TglRencanaKontrol)
+    {
+        $response = $this->get('RencanaKontrol/ListSpesialistik/JnsKontrol/'.$JnsKontrol.'/nomor/'.$nomor.'/TglRencanaKontrol/'.$TglRencanaKontrol);
+        return json_decode($response, true);
+    }
+    
+    public function listJadwalDokter($JnsKontrol, $KdPoli, $TglRencanaKontrol)
+    {
+        $response = $this->get('RencanaKontrol/JadwalPraktekDokter/JnsKontrol/'.$JnsKontrol.'/KdPoli/'.$KdPoli.'/TglRencanaKontrol/'.$TglRencanaKontrol);
+        return json_decode($response, true);
+    }
+}
